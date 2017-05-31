30417201f5a4ffec895edef27144a3c948f0efbd
Aaron Boatwright <boatwrightaaron@gmail.com>
2017-05-30 18:13:56 -0700

    first commit

---
+++ chess-react/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component }  from 'react'
+import { HashRouter, Route } from 'react-router-dom'
+
+import './App.css'
+import Home from '/.Home'
+import Game from '/.Game'
+
+export default class App extends Component {
+  render()
+    return (
+      <HashRouter>
+    )
+}
